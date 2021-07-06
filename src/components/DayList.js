55b1fdf11890eb53ed52cffd759327eddc334307
Byeongjae Kang <bart.kang.0719@gmail.com>
2021-07-05 23:29:55 -0700

    refactor code

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,27 +1,9 @@
 import React from 'react';
 import DayListItem from './DayListItem';
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
-
 export default function DayList(props){
 
-  const rendorDays = days.map(day => {
+  const rendorDays = props.days.map(day => {
     return <DayListItem 
     name={day.name} 
     spots={day.spots} 
