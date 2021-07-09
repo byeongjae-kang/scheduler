a5c517d715bdbc406454c18154b73b0570ddcb4f
Byeongjae Kang <bart.kang.0719@gmail.com>
2021-07-09 15:02:27 -0700

    get save function as prop and add to onClick event

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -12,11 +12,9 @@ export default function Form(props) {
   }
 
   const cancel = () => {
-
     reset();
     props.onCancel()
   }
-  
   
   return (
     <main className="appointment__card appointment__card--create">
@@ -29,10 +27,6 @@ export default function Form(props) {
             placeholder="Enter Student Name"
             value={name}
             onChange={(event) => setName(event.target.value)}
-
-            /*
-              This must be a controlled component
-            */
           />
         </form>
         <InterviewerList interviewers={props.interviewers} interviewer={interviewer} setInterviewer={setInterviewer} />
@@ -40,7 +34,7 @@ export default function Form(props) {
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={cancel}>Cancel</Button>
-          <Button confirm onClick={props.onSave}>Save</Button>
+          <Button confirm onClick={() => props.onSave(name, interviewer)}>Save</Button>
         </section>
       </section>
     </main>
