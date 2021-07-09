5d91acfc8deff0f5b75ba55dd655c53478f407ff
Byeongjae Kang <bart.kang.0719@gmail.com>
2021-07-08 23:27:04 -0700

    add helper functions and test them

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,9 +1,20 @@
-export function getAppointmentsForDay(state, day) {
+const getAppointmentsForDay = (state, day) => {
   const appointments = [];
   
   state.days.filter(aDay => aDay.name === day)
             .forEach(aDay => aDay.appointments.forEach(id => appointments.push(state.appointments[id])));
             
   return appointments;
-  //... returns an array of appointments for that day
 }
+
+const getInterview = (state, interview) => {
+  const interviewers = state.interviewers
+  const interviewerNumber = interview && interview.interviewer;
+  if (interviewerNumber && interviewers[interviewerNumber]) {
+    interview.interviewer = interviewers[interviewerNumber]
+    return interview;
+  }
+  return null
+};
+
+module.exports = { getAppointmentsForDay, getInterview }
