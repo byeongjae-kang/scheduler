4bf6e5a7b838cc7a75e5d7f99ac2bd77d9afcf9c
Byeongjae Kang <bart.kang.0719@gmail.com>
2021-07-09 01:12:28 -0700

    implement useVisualMode function and render components

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,19 +3,42 @@ import 'components/Appointment/styles.scss';
 import Header from './Header';
 import Empty from './Empty';
 import Show from './Show';
+import Form from './Form';
+import useVisualMode from 'hooks/useVisualMode';
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
+
+
 
 export default function Appointment(props) {
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+  console.log(props.interview)
+  return (
+    <article className="appointment">
+      <Header time={props.time} />
 
-  return ( 
-  <article className="appointment">
-    <Header time={props.time} />
-    {props.interview ? 
-      <Show 
-        student={props.interview.student}
-        interviewer={props.interview.interviewer}
-      /> : 
-      <Empty/>
-    }
-  </article>
+      {mode === EMPTY && 
+        <Empty 
+          onAdd={() => transition(CREATE)} 
+        />
+      }
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      )}
+      {mode === CREATE && (
+        <Form
+          interviewers={props.interviewers}
+          // onSave={onSave}
+          onCancel={() => back()}
+        />
+      )}
+    </article>
   );
 }
